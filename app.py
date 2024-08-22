14a8b354dcf5408585de28c2cd24e7311d03d609
tintinthedev <randomthecoder1557@gmail.com>
2024-08-22 14:59:46 -0300

    feat: create barebones functionality
    The app works, but is not pretty and can still
    have some bugs that need to be fixed.
    
    The results (expired, not expired or expired today)
    are only shown in the terminal. They should be
    displayed at the GUI.
    
    Also, the code needs to be refactored.

---
+++ app.py
@@ -0,0 +1,103 @@
+import customtkinter as ctk
+import cv2
+from CTkMessagebox import CTkMessagebox
+import pytesseract
+import re
+import datetime
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.camera_capture: None | cv2.VideoCapture = None
+
+        self.geometry("600x600")
+        self.title("Expiration date verifier")
+
+        self.create_widgets()
+        self.draw_widgets()
+
+        self.mainloop()
+
+    def create_widgets(self):
+        self.activate_camera_button = ctk.CTkButton(
+            self, text="Activate camera", command=self.activate_camera
+        )
+
+    def draw_widgets(self):
+        self.activate_camera_button.place(relx=0.5, rely=0.5, anchor="center")
+
+    def find_expiration_date_in_text(self, text: str):
+        # TODO: Add the possibility for the user to choose the date format
+        # on the app's menu
+
+        print(text)
+
+        # TODO: Maybe find a better way to get the exp. date using regex
+        expiration_date = re.findall(
+            r"\b(?:val|VAL)\s+(\d{2})/(\d{2})/(\d{2}|\d{4})\b", text
+        )
+
+        if len(expiration_date) == 0:
+            return
+
+        # [('21', '10', '24')]
+        expiration_day = expiration_date[0][0]
+        expiration_month = expiration_date[0][1]
+        expiration_year = expiration_date[0][2]
+
+        # FIX: Find a way to make it work without hard coding the first 2 digits of the year
+        if len(expiration_year) == 2:
+            expiration_year = f"20{expiration_year}"
+
+        today = datetime.date.today()
+        expiration_date = datetime.date(
+            int(expiration_year), int(expiration_month), int(expiration_day)
+        )
+
+        print(today, expiration_date)
+
+        if expiration_date == today:
+            print("EXPIRES TODAY")
+        elif expiration_date > today:
+            print("DID NOT EXPIRE")
+        else:
+            print("EXPIRED")
+
+    def activate_camera(self):
+        self.check_for_cameras()
+
+        while True:
+            ret, frame = self.camera_capture.read()
+
+            if not ret:
+                # TODO: Warn the user about the disconnected camera
+                self.camera_capture.release()
+                break
+
+            grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            frame_text = pytesseract.image_to_string(grayscale_frame)
+            self.find_expiration_date_in_text(frame_text)
+            # print(frame_text)
+
+            cv2.imshow("frame", grayscale_frame)
+
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+    def check_for_cameras(self):
+        self.camera_capture = cv2.VideoCapture(0)
+
+        if not self.camera_capture.isOpened():
+            no_camera_answer = CTkMessagebox(
+                title="ERROR",
+                message="No camera available. Please connect a camera.",
+                option_1="Quit",
+                option_2="Ok",
+            )
+
+            if no_camera_answer.get() == "Quit":
+                exit()
+
+            self.check_for_cameras()
